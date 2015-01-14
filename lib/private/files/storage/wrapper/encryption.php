5643ee43586789e661ed4b43457f245e45223121
Bjoern Schiessle <schiessle@owncloud.com>
2015-01-14 20:39:23 +0100

    create basic interfaces and wrapper to make encryption more modular

---
+++ lib/private/files/storage/wrapper/encryption.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * ownCloud
+ *
+ * @copyright (C) 2015 ownCloud, Inc.
+ *
+ * @author Bjoern Schiessle <schiessle@owncloud.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OC\Files\Storage\Wrapper;
+
+class Encryption extends Wrapper {
+
+	private $mountPoint;
+
+	/**
+	 * @param array $parameters
+	 */
+	function __construct($parameters) {
+		$this->mountPoint = $parameters['mountPoint'];
+		parent::__construct($parameters);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.filesize.php
+	 * The result for filesize when called on a folder is required to be 0
+	 *
+	 * @param string $path
+	 * @return int
+	 */
+	public function filesize($path) {
+		$fullPath = $this->getFullPath($path);
+		$fileInfo = \OC\Files\Filesystem::getFileInfo($fullPath);
+		$size = $fileInfo->getSize();
+		if ($fileInfo->getSize() > 0 && $fileInfo->isEncrypted()) {
+			$size = $fileInfo->getUnencryptedSize();
+			if ($size <= 0) {
+				$encryptionModuleId = $util->getEncryptionModuleId();
+				/* @var $encryptionManager \OC\Encryption\Manager */
+				$encryptionManager = \OC::$server->getEncryptionManager();
+				$encryptionModule = $encryptionManager->getEncryptionModule($encryptionModuleId);
+				$size = $encryptionModule->calculateUnencryptedSize($fullPath);
+				\OC\Files\Filesystem::putFileInfo($fullPath, array('unencrypted_size' => $size));
+			}
+
+		}
+
+		return $size;
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.file_get_contents.php
+	 *
+	 * @param string $path
+	 * @return string
+	 */
+	public function file_get_contents($path) {
+		// todo drecrypt data
+		return $this->storage->file_get_contents($path);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.file_put_contents.php
+	 *
+	 * @param string $path
+	 * @param string $data
+	 * @return bool
+	 */
+	public function file_put_contents($path, $data) {
+		//todo encrypt data
+		return $this->storage->file_put_contents($path, $data);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.unlink.php
+	 *
+	 * @param string $path
+	 * @return bool
+	 */
+	public function unlink($path) {
+		//todo remove encryption keys
+		return $this->storage->unlink($path);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.rename.php
+	 *
+	 * @param string $path1
+	 * @param string $path2
+	 * @return bool
+	 */
+	public function rename($path1, $path2) {
+		// todo rename encryption keys, get users with access to the file and reencrypt
+		// or is this to encryption module specific? Then we can hand this over
+		return $this->storage->rename($path1, $path2);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.copy.php
+	 *
+	 * @param string $path1
+	 * @param string $path2
+	 * @return bool
+	 */
+	public function copy($path1, $path2) {
+		// todo copy encryption keys, get users with access to the file and reencrypt
+		// or is this to encryption module specific? Then we can hand this over
+		return $this->storage->copy($path1, $path2);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.fopen.php
+	 *
+	 * @param string $path
+	 * @param string $mode
+	 * @return resource
+	 */
+	public function fopen($path, $mode) {
+		return $this->storage->fopen($path, $mode);
+	}
+
+	/**
+	 * return full path, including mount point
+	 *
+	 * @param string $path relative to mount point
+	 * @return string full path including mount point
+	 */
+	protected function getFullPath($path) {
+		return Filesystem::normalizePath($this->mountPoint . '/' . $path);
+	}
+
+}
