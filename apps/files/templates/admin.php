a75ea2cde8417dfdae53ed135975c6da3a9841b6
Musa <amusa32@gmail.com>
2015-02-24 15:34:54 +0100

    file type restriction
    file type restriction

--- apps/files/templates/admin.php
+++ apps/files/templates/admin.php
@@ -20,6 +20,17 @@
 	<?php OCP\Util::addscript('files', 'extended'); ?>
 	<form name="extendedSettings" class="section" action="#" method="post">
 		<h2><?php p($l->t('File handling')); ?></h2>
+		<!-- file type restriction -->
+		<p class="">
+			<input id="fileTypeRestriction" type="checkbox" <?php if ($_['fileTypeRes'] === 'yes') print_unescaped('checked="checked"');?> name="filetyperes_enabled" original-title="">
+			<label for="fileTypeRestriction">Enable file type restriction</label>
+			<br/>
+		</p>
+		<p id="restriction" class="indent <?php if ($_['fileTypeRes'] === 'no') p('hidden');?>">
+		<label for="filetypes">Allowed filetypes </label>
+		<input type="text" name="allowed_filetypes" id="filetypes" value='<?php p($_['allowed_filetypes']) ?>'/>
+		</p>
+		<!-- /file type restriction  -->
 		<label for="delete">Users in this group <b>can delete</b>  files.</label><br />
 		<input name="delete" type="hidden" class="uploadGroups" value="<?php p($_['deleteGroupsList']) ?>" style="width: 400px"/><br />
 		<em>These groups will be able to delete files.</em>
