f20e4bfbc511ce4c065bbfab52fd5e5716c56d06
Musa <amusa32@gmail.com>
2015-02-24 15:30:52 +0100

    file type restriciton
    file type restriciton

--- apps/files/js/extended.js
+++ apps/files/js/extended.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-  $('.uploadGroups').each(function (index, element) {
-    OC.Settings.setupGroupsSelect($(element));
-    $(element).change(function(ev) {
-      var groups = ev.val || [];
+	$('#fileTypeRestriction').change(function() {
+		OC.AppConfig.setValue('core', $(this).attr('name'), this.checked ? 'yes' : 'no');
+		$("#restriction").toggleClass('hidden', !this.checked);
+	});
+	$('.uploadGroups').each(function (index, element) {
+		OC.Settings.setupGroupsSelect($(element));
+		$(element).change(function(ev) {
+			var groups = ev.val || [];
 			groups = (groups.length > 0) ? ev.val.join(',') : '';
 			OC.AppConfig.setValue('core', $(this).attr('name'), groups);
 		});
+	});
+	$('#filetypes').change(function(){
+		OC.AppConfig.setValue('core', $(this).attr('name'), $(this).attr('value'));
 	});
 });
